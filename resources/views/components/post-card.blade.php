20dbdf335b986c52391ecfee68511dd1194b53c1
Esther Massarioli <esther.massarioli@luizalabs.com>
2022-01-27 19:02:30 -0300

    estrutura basica

---
+++ resources/views/components/post-card.blade.php
@@ -0,0 +1,17 @@
+<div class="card mb-3" style="width: 600px; max-width: 100%;">
+
+    <div class="card-header bg-white border-0">
+        <i class="bi bi-person-circle fs-5 me-2"></i>
+        <span class="fw-bold">João Silva</span>
+    </div>
+
+    <img src="https://i.imgur.com/BEqFxWf.png" class="card-img rounded-0" alt="">
+
+    <div class="card-body">
+        <p class="card-text">
+            <span class="fw-bold">João Silva</span> 
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum eveniet error nihil aperiam. Temporibus sed maxime repellat inventore accusamus animi quibusdam corporis exercitationem, ea, atque odio sit ratione error eligendi?
+        </p>
+    </div>
+
+</div>
